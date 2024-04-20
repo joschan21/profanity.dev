161045afd145ffe2337c2fdf9840fd13a35d4ba3
Joscha <neske.joscha@gmail.com>
2024-04-20 10:52:55 +0200

    ui: add github button

--- www/src/components/Navbar.tsx
+++ www/src/components/Navbar.tsx
@@ -1,10 +1,12 @@
+import { Heart } from 'lucide-react'
 import Link from 'next/link'
+import { buttonVariants } from './ui/button'
 
 const Navbar = () => {
   return (
     <div className='sticky inset-x-0 top-0 z-30 w-full transition-all border-b border-gray-200 bg-white/75 backdrop-blur-lg'>
       <div className='max-w-7xl mx-auto lg:px-8 px-6'>
-        <div className='relative flex h-14 items-center justify-between sm:justify-center'>
+        <div className='relative flex h-14 items-center justify-between'>
           <Link
             href='/'
             className='relative sm:absolute inset-y-0 left-0 flex items-center font-semibold'>
@@ -12,7 +14,10 @@ const Navbar = () => {
             ProfanityAPI
           </Link>
 
-          <div className='flex items-center gap-6'>
+          {/* placeholder */}
+          <div className='hidden sm:block invisible'>ProfanityAPI</div>
+
+          <div className='hidden sm:flex items-center gap-6'>
             <Link className='hover:underline' href='#video-demo'>
               Video Demo
             </Link>
@@ -20,6 +25,14 @@ const Navbar = () => {
               API
             </Link>
           </div>
+
+          <Link
+            href='https://github.com/joschan21/profanity.dev'
+            target='_blank'
+            referrerPolicy='no-referrer'
+            className={buttonVariants({ variant: 'secondary' })}>
+            Star on GitHub <Heart className='h-4 w-4 ml-1.5 fill-primary' />
+          </Link>
         </div>
       </div>
     </div>
