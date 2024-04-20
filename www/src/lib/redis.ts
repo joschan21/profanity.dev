f1a6482cebd9aa20c89138e806b4e3eb72dc8477
Mwelwa Nkuta <mwelwankuta@gmail.com>
2024-04-20 18:57:35 +0200

    refactor: error for absent environment variables

--- www/src/lib/redis.ts
+++ www/src/lib/redis.ts
@@ -1,6 +1,13 @@
 import { Redis } from '@upstash/redis'
 
+const token = process.env.UPSTASH_REDIS_REST_TOKEN
+const url = process.env.UPSTASH_REDIS_REST_URL
+
+if(!url || !token) {
+  throw new Error('missing the following environment variables: \nUPSTASH_REDIS_REST_URL=\nUPSTASH_REDIS_REST_TOKEN')
+}
+
 export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+  url,
+  token
 })
