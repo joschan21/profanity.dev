4eb19f9ba2f7b58f34133939820bd9f80d28c667
elgeneee <eedren@gmail.com>
2024-04-24 23:20:29 +0800

    feat: add metadata, add button loading state

--- www/src/lib/utils.ts
+++ www/src/lib/utils.ts
@@ -1,6 +1,51 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { Metadata } from "next"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function constructMetadata({
+  title = 'Fast, Open-Source Profanity API',
+  description = 'A project made by JoshTriedCoding',
+  image = '/thumbnail.jpg',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@joshtriedcoding',
+    },
+    icons,
+    metadataBase: new URL('https://www.profanity.dev'),
+    themeColor: '#FFF',
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
 }
