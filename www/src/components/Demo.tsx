a19954b4f2208551908ecea51565618869836cc8
Oscar <83197788+Wosca@users.noreply.github.com>
2024-05-10 20:29:53 +1000

    UI Improvements
    Added correct alt text on home page, and made powered by upstash link to the upstash site.

--- www/src/components/Demo.tsx
+++ www/src/components/Demo.tsx
@@ -1,67 +1,68 @@
-'use client'
+"use client";
 
-import { checkProfanity } from '@/actions'
-import { useMutation } from '@tanstack/react-query'
-import { useState } from 'react'
-import { Button } from './ui/button'
-import { Input } from './ui/input'
-import { Icons } from './Icons'
+import { checkProfanity } from "@/actions";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+import { Icons } from "./Icons";
 
 const Demo = () => {
   const [message, setMessage] = useState<string>(
-    'this is definitely not a swear word'
-  )
+    "this is definitely not a swear word"
+  );
 
   const { data, mutate, isPending, error } = useMutation({
-    mutationKey: ['check-profanity'],
+    mutationKey: ["check-profanity"],
     mutationFn: checkProfanity,
     onSettled: (data) => {
-      if (data && 'error' in data) {
-        throw new Error(data.error)
+      if (data && "error" in data) {
+        throw new Error(data.error);
       }
     },
-  })
+  });
 
-  const successData = data && !('error' in data) ? data : undefined
+  const successData = data && !("error" in data) ? data : undefined;
 
   return (
-    <div className='flex flex-col gap-5 items-center'>
-      <div className='relative w-full rounded-xl mt-12 bg-gray-900/5 p-4 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl'>
-        <div className='flex justify-between items-center'>
-          <div className='flex items-center gap-4'>
-            <span className='inline-flex items-center rounded-md bg-zinc-700 px-3 py-1 text-xs font-medium text-white ring-1 ring-inset ring-blue-400/20'>
+    <div className="flex flex-col gap-5 items-center">
+      <div className="relative w-full rounded-xl mt-12 bg-gray-900/5 p-4 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-4">
+            <span className="inline-flex items-center rounded-md bg-zinc-700 px-3 py-1 text-xs font-medium text-white ring-1 ring-inset ring-blue-400/20">
               POST
             </span>
-            <div className='h-[20px] w-px bg-zinc-300' />
-            <p className='break-all'>https://vector.profanity.dev</p>
+            <div className="h-[20px] w-px bg-zinc-300" />
+            <p className="break-all">https://vector.profanity.dev</p>
           </div>
         </div>
-        <div className='relative flex flex-col sm:flex-row items-center gap-2 mt-6 h-full sm:h-9'>
+        <div className="relative flex flex-col sm:flex-row items-center gap-2 mt-6 h-full sm:h-9">
           <Input
-            className='bg-white h-9'
+            className="bg-white h-9"
             value={message}
             onKeyDown={(e) => {
-              if (e.key === 'Enter') mutate({ message })
+              if (e.key === "Enter") mutate({ message });
             }}
             onChange={({ target }) => {
-              setMessage(target.value)
+              setMessage(target.value);
             }}
           />
           <Button
             disabled={isPending}
-            className='h-9 w-full sm:w-fit'
-            onClick={() => mutate({ message })}>
+            className="h-9 w-full sm:w-fit"
+            onClick={() => mutate({ message })}
+          >
             Profanity check
           </Button>
         </div>
 
-        <div className='h-32 mt-4 rounded-lg border-2 border-dashed border-zinc-300 text-sm flex items-center justify-center'>
+        <div className="h-32 mt-4 rounded-lg border-2 border-dashed border-zinc-300 text-sm flex items-center justify-center">
           {successData ? (
-            <div className='flex flex-col items-center text-center'>
-              <p className='font-bold'>
+            <div className="flex flex-col items-center text-center">
+              <p className="font-bold">
                 {successData.score > 0.95 ? (
                   <span>
-                    🚨🚨😱😱 OH GOD, VERY BIG PROFANITY DETECTED!! 🚨🚨😱😱{' '}
+                    🚨🚨😱😱 OH GOD, VERY BIG PROFANITY DETECTED!! 🚨🚨😱😱{" "}
                   </span>
                 ) : successData.score > 0.9 ? (
                   <span>🚨😱 BIG PROFANITY DETECTED!! 🚨😱 </span>
@@ -74,27 +75,30 @@ const Demo = () => {
                 ) : null}
               </p>
 
-              <p className='text-sm text-zinc-700'>
+              <p className="text-sm text-zinc-700">
                 score (higher is worse): {successData.score.toFixed(3)}
               </p>
             </div>
           ) : (
-            <p className='text-zinc-700'>Results will be shown here</p>
+            <p className="text-zinc-700">Results will be shown here</p>
           )}
         </div>
 
         {error ? (
-          <p className='text-red-600 text-sm mt-2'>
-            <span className='font-semibold'>Error:</span> {error.message}
+          <p className="text-red-600 text-sm mt-2">
+            <span className="font-semibold">Error:</span> {error.message}
           </p>
         ) : null}
       </div>
 
-      <div className='text-sm flex gap-2 items-center h-fit'>
-        powered by <Icons.upstash className='w-20' />
-      </div>
+      <a
+        href="https://upstash.com/"
+        className="text-sm flex gap-2 items-center h-fit"
+      >
+        powered by <Icons.upstash className="w-20" />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
